c748e0bc3adac46d2632aa504b54bd1ac8daaca1
hongyiliu <wenhualiu.liu@gmail.com>
2019-06-16 21:58:56 -0700

    add controller with post/get request

---
+++ src/posts/post.dto.ts
@@ -0,0 +1,4 @@
+export class CreatePostsDto {
+    readonly title: string;
+
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Query,Headers, Param, Post, Body } from '@nestjs/common';
+import { CreatePostsDto } from './post.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {  
+    @Get()
+    index(@Headers() headers) {
+        console.log(headers);
+        return [
+            {title:'hello~'}
+        ];
+    }
+    @Get(':id')
+    show(@Param() params) {
+        return {
+            title: `Post ${params.id}`
+        }
+    }
+
+    @Post()
+    store(@Body() post:CreatePostsDto) {
+        console.log(post.title);
+
+    }
+
+}
